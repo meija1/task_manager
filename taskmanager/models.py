75d1446e083fc364e7da6eb7d8dcd0985295fbb5
meija1 <meija911@gmail.com>
2023-05-12 15:40:17 +0000

    Add Edit and Delete function to categories

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -7,9 +7,7 @@ class Category(db.Model):
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        return "#{0} - Task: {1} | Urgent: {2}".format(
-            self.id, self.task_name, self.is_urgent
-        )
+        return self.category_name
 
 
 class Task(db.Model):
@@ -19,3 +17,9 @@ class Task(db.Model):
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
+
+def __repr__(self):
+    return "#{0} - Task: {1} | Urgent: {2}".format(
+        self.id, self.task_name, self.is_urgent
+    )
